4fc8a8dafba4e474904e1f3b1b9477ce137e152d
ameyflash <amey.hema@gmail.com>
2023-10-22 01:26:50 -0600

    Added A7 changes

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-LDD_VERSION = 131189466cbeb2a35074e3260906d79a6f61c987
+LDD_VERSION = be5d088ac32491f339b853956aff95d9c2936468
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
@@ -24,4 +24,5 @@ define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin/
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
