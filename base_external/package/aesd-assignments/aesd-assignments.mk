780c4aef4d4716e0c453a03f90cf02352e958b68
ameyflash <amey.hema@gmail.com>
2023-10-31 20:42:20 -0600

    AESD char driver additions

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f69a4d2cd5bfa9448936eb3168d8b15b542ae43f
+AESD_ASSIGNMENTS_VERSION = 493dec81fc00fb8f1dfc08ef4d2bb8ef8d82eea0
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
